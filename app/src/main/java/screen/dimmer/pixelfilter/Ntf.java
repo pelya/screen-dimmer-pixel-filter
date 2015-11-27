a7af35c19c83c90c46e5c47ab4bbfd0008957fca
Robert LaFont, Jr <bobby.lafont@gmail.com>
2015-11-27 13:45:51 -0800

    Improved Notification
    Fixes Issue #3

--- app/src/main/java/screen/dimmer/pixelfilter/Ntf.java
+++ app/src/main/java/screen/dimmer/pixelfilter/Ntf.java
@@ -4,10 +4,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
 import android.widget.RemoteViews;
 
 public class Ntf {
@@ -26,22 +24,33 @@ public class Ntf {
             ntfView.setOnClickPendingIntent(R.id.notificationConfigure, edit);
             ntfView.setOnClickPendingIntent(R.id.notificationIcon, cancel);
             ntfView.setOnClickPendingIntent(R.id.notificationText, cancel);
-
-            Notification.Builder builder = new Notification.Builder(ctx)
-                    .setContent(ntfView)
+            NotificationCompat.Action.Builder ntf_act_configure = new NotificationCompat.Action.Builder(R.drawable.ic_build_black, ctx.getResources().getString(R.string.configure), edit);
+            NotificationCompat.Action.Builder ntf_act_disable = new NotificationCompat.Action.Builder(R.drawable.ic_close, ctx.getResources().getString(R.string.disable), cancel);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx)
+                    .setContentTitle(ctx.getResources().getString(R.string.app_name))
+                    .setContentInfo(ctx.getResources().getString(R.string.tap_to_configure))
+                    //.setContent(ntfView)
+                    .setContentIntent(edit)
+                    .addAction(ntf_act_configure.build())
+                    .addAction(ntf_act_disable.build())
                     .setDeleteIntent(cancel)
-                    .setPriority(Notification.PRIORITY_LOW)
+                    .setPriority(NotificationCompat.PRIORITY_MIN)
                     .setSmallIcon(R.drawable.notification)
                     .setSound(null)
                     .setOngoing(true)
+                    .setLocalOnly(true)
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                     .setTicker(null);
 
+<<<<<<< HEAD
+=======
             // Using reflection so the app will not crash on lower API devices
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 builder.setLocalOnly(true);
                 builder.setVisibility(Notification.VISIBILITY_PUBLIC);
             }
 
+>>>>>>> 02ba4404b7b3ac0298015eda8ec3229725338407
             Notification ntf = builder.build();
             ctx.startForeground(NTF_ID, ntf);
         } else {
