57436ef986559d39c578ca12eec7da9784ca7cd6
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-01-11 22:01:40 +0200

    Fixed Gradle wrapper script

--- sign.sh
+++ sign.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-#./gradlew assembleRelease || exit 1
+./gradlew assembleRelease || exit 1
 
 [ -z "$ANDROID_KEYSTORE_FILE" ] && ANDROID_KEYSTORE_FILE=~/.android/debug.keystore
 [ -z "$ANDROID_KEYSTORE_ALIAS" ] && ANDROID_KEYSTORE_ALIAS=androiddebugkey
 
 APPNAME=PixelFilter
 
+rm -f $APPNAME.apk
+
+zipalign 4 app/build/outputs/apk/release/app-release-unsigned.apk $APPNAME.apk
+
 stty -echo
 #jarsigner -verbose -keystore $ANDROID_KEYSTORE_FILE -sigalg MD5withRSA -digestalg SHA1 app/build/outputs/apk/app-release-unsigned.apk $ANDROID_KEYSTORE_ALIAS || exit 1
-apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS app/build/outputs/apk/release/app-release-unsigned.apk
+apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS $APPNAME.apk
 stty echo
 echo
-rm -f $APPNAME.apk
-zipalign 4 app/build/outputs/apk/release/app-release-unsigned.apk $APPNAME.apk
