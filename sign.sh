b5e47ba2acb21ee484773c52391e77532fef03e0
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-01-11 21:46:39 +0200

    Updated sign.sh to use apksigner, updated Gradle scripts again

--- sign.sh
+++ sign.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./gradlew assembleRelease || exit 1
+#./gradlew assembleRelease || exit 1
 
 [ -z "$ANDROID_KEYSTORE_FILE" ] && ANDROID_KEYSTORE_FILE=~/.android/debug.keystore
 [ -z "$ANDROID_KEYSTORE_ALIAS" ] && ANDROID_KEYSTORE_ALIAS=androiddebugkey
@@ -8,8 +8,9 @@
 APPNAME=PixelFilter
 
 stty -echo
-jarsigner -verbose -keystore $ANDROID_KEYSTORE_FILE -sigalg MD5withRSA -digestalg SHA1 app/build/outputs/apk/app-release-unsigned.apk $ANDROID_KEYSTORE_ALIAS || exit 1
+#jarsigner -verbose -keystore $ANDROID_KEYSTORE_FILE -sigalg MD5withRSA -digestalg SHA1 app/build/outputs/apk/app-release-unsigned.apk $ANDROID_KEYSTORE_ALIAS || exit 1
+apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS app/build/outputs/apk/release/app-release-unsigned.apk
 stty echo
 echo
 rm -f $APPNAME.apk
-zipalign 4 app/build/outputs/apk/app-release-unsigned.apk $APPNAME.apk
+zipalign 4 app/build/outputs/apk/release/app-release-unsigned.apk $APPNAME.apk
